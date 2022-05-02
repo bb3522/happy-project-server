c6603face632b0a13f78383ec9834f7209cfd735
ratih sanjaya <ratihsanjayaw@gmail.com>
2022-05-03 01:40:08 +0700

    test 2

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+const port = process.env.PORT || 3000;
+
 const express = require("express");
 const cors = require("cors");
 const { errorHandler } = require("./middlewares/errorHandler");
@@ -19,4 +21,6 @@ app.use("/", route);
 
 app.use(errorHandler);
 
-module.exports = app
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
